a1473894d1f5654a74574b2e97e836d2c1c3b567
pimpinho16 <alexander.amedina@gmail.com>
2021-06-20 21:16:18 -0600

    Ejemplo de propiedades computadas

--- codigo.js
+++ codigo.js
@@ -2,6 +2,28 @@ const app = new Vue({
     el: '#main',
     data:{
        conectado:false,
-       edad :17
+       edad :17,
+       lista: ['Manuel','Jose','Victoria'],
+       listaPersonas:[
+           {nombre:'Manuel',edad:37},
+           {nombre:'Jose',edad:25},
+           {nombre:'Victoria',edad:30}
+       ],
+       tareas:[
+           {titulo:'Aprender ES6',completado:false},
+           {titulo:'Aprender Vue',completado:true}, 
+           {titulo:'Descansar toda la tarde',completado:false}, 
+        ],
+       tarea: '',
+    },
+    methods:{
+        agregarTarea(){
+            this.tareas.unshift( {titulo:this.tarea,completado:true}) 
+        }
+    },
+    computed:{
+        mostrarCompletadas(){
+            return this.tareas.filter(item => item.completado)
+        }
     }
 })
